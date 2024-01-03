74dbc090180965bbe59cf19c30916a895e479b56
Dibyas98 <dibyashaktip98.com>
2024-01-03 15:51:58 +0530

    project.css

--- src/component/right/project/MyProject.jsx
+++ src/component/right/project/MyProject.jsx
@@ -8,12 +8,14 @@ import bayeraskmed from './Assects/bayer-askmed.png'
 import enemzet from './Assects/enemzet.png'
 import tinnitusblog from './Assects/tinnitus-blog.png'
 import ButtomNext from '../Heaading/ButtomNext'
+import './project.css'
 
 export default function MyProject() {
   return (
     <div className='pb-10'>
       <Heading head={'My Projects'}></Heading>
-      <div className='p-8 flex flex-wrap gap-5 justify-between'>
+      <div className='flex justify-center '>
+      <div className=' proj-card p-1 pt-10 flex flex-wrap gap-5 justify-between   w-11/12'>
         <ProjectCard pic={mybestwishes} name={'MyBestWishes AI Generator'} des ={`Design + Frontend Figma | React.js | Chakra UI`} show={`Preview`}/>
         <ProjectCard pic={myworkphoto} name={'MyWorkPhoto AI Generator'} des ={`Design + Frontend Figma | React.js | Chakra UI`} show={`Preview`}/>
         <ProjectCard pic={ognodashboard} name={'Ogno Marketing Dashboard'} des ={`Fullstack Project React.js | Strapi.js | MySQL`} show={`GitHub`}/>
@@ -23,6 +25,7 @@ export default function MyProject() {
         <ProjectCard pic={mybestwishes} name={'MyBestWishes AI Generator'} des ={`Design + Frontend Figma | React.js | Chakra UI`} show={`Preview`}/>
         <ProjectCard pic={mybestwishes} name={'MyBestWishes AI Generator'} des ={`Design + Frontend Figma | React.js | Chakra UI`} show={`Preview`}/>
         <ProjectCard pic={mybestwishes} name={'MyBestWishes AI Generator'} des ={`Design + Frontend Figma | React.js | Chakra UI`} show={`Preview`}/>
+      </div>
       </div>
       <ButtomNext></ButtomNext>
     </div>
