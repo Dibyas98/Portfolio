608fbf30a8d1afb5b36618291ca77ca6cb3cffbf
Dibyas98 <dibyashaktip98.com>
2024-01-03 17:56:57 +0530

    poin

--- src/component/right/nav/nav.jsx
+++ src/component/right/nav/nav.jsx
@@ -10,11 +10,11 @@ export default function Nav(props) {
     <div className='parent-container relative text-[#FFC663]'>
       <div className='navbar bg-[#284249] w-3/5 float-right h-16 flex items-center px-10 rounded-bl-lg fixed-child'>
         <ul className='flex w-full justify-between text-l font-semibold px-2'>
-          <li className='' onClick={HandelMenu}>About</li>
-          <li onClick={HandelMenu}>Certificates</li>
-          <li onClick={HandelMenu}>Projects</li>
-          <li onClick={HandelMenu}>More</li>
-          <li onClick={HandelMenu}>Contact</li>
+          <li className='cursor-pointer' onClick={HandelMenu}>About</li>
+          <li className='cursor-pointer' onClick={HandelMenu}>Certificates</li>
+          <li className='cursor-pointer' onClick={HandelMenu}>Projects</li>
+          <li className='cursor-pointer' onClick={HandelMenu}>More</li>
+          <li className='cursor-pointer' onClick={HandelMenu}>Contact</li>
         </ul>
       </div>
     </div>
