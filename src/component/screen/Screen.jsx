324374cf6c3e8835587b533a5a4c5f62d0f3321e
Dibyas98 <dibyashaktip98.com>
2024-01-03 17:54:56 +0530

    link

--- src/component/screen/Screen.jsx
+++ src/component/screen/Screen.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
-import Left from '../left/Left'
-import './screen.css'
-import Nav from '../right/nav/nav'
-import Heading from '../right/Heaading/Heading'
-import About from '../right/AboutMe/About'
-import Certificates from '../right/Certi/Certificates'
-import MyProject from '../right/project/MyProject'
-import More from '../right/More/More'
+import React, { useState } from 'react';
+import Left from '../left/Left';
+import './screen.css';
+import Nav from '../right/nav/nav';
+import About from '../right/AboutMe/About';
+import Certificates from '../right/Certi/Certificates';
+import MyProject from '../right/project/MyProject';
+import More from '../right/More/More';
 
 export default function Screen() {
+  const [live, setLive] = useState('About'); 
+
+  const handleMenuClick = (section) => {
+    console.log(section);
+    setLive(section);
+  };
+
+  const renderSection = () => {
+    switch (live) {
+      case 'About':
+        return <About />;
+      case 'Certificates':
+        return <Certificates></Certificates>;
+      case 'Projects':
+        return <MyProject/>
+      case 'More':
+        return <More/>
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div className='w-full   bg-[#23D3Ff] flex justify-center pt-10 pb-14  gap-10 cont'>
-        <div className=' w-1/6 bg-[#21839C] py-16 rounded-2xl h-3/4 left'>
-            <Left></Left>
-        </div>
-        <div className='bg-[#235777] rounded-xl w-3/5  right'>
-          <Nav></Nav>
-          {/* <About></About> */}
-          {/* <Certificates></Certificates> */}
-          {/* <MyProject></MyProject> */}
-          <More>  </More>
-        </div>
-      
+    <div className='w-full bg-[#23D3Ff] flex justify-center pt-10 pb-14 gap-10 cont'>
+      <div className='w-1/6 bg-[#21839C] py-16 rounded-2xl h-3/4 left'>
+        <Left />
+      </div>
+      <div className='bg-[#235777] rounded-xl w-3/5 right'>
+        <Nav onMenuClick={handleMenuClick} />
+        {renderSection()}
+      </div>
     </div>
-  )
+  );
 }
